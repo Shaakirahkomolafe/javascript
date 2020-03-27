cd3192321b24b0c035afd24d80a88a4acc8157b0
[shaakirahkomolafe] <[damolakomolafe.dk@gmail.com]>
2020-03-27 23:46:51 +0100

    newjs

---
+++ index.js
@@ -0,0 +1,20 @@
+var name = "shaakirah";
+
+console.log(name);
+
+let courses = ["html, css, javascript,Reactjs"];
+
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     courses.push(i);
+
+   }
+
+ }
+
+ console.log(courses);
+
+ 
